d64cac18b3f1351e4aa2513b54f1cbaafc8580af
David <david@templatious.org>
2015-04-12 08:28:17 +0300

    high speed low drag

---
+++ main.cpp
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  Main unit test ground
+ *
+ *        Version:  1.0
+ *        Created:  07/27/2014 05:38:18 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  David Kazlauskas (dk), david@templatious.org
+ *
+ * =====================================================================================
+ */
+
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+
+#include "TestDefs.hpp"
+
+TEST_CASE("lol","[lol]")
+{
+    REQUIRE( tt::fullRun<std::vector,std::allocator>() );
+}
+
+int main( int argc, char* const argv[] )
+{
+    int result = Catch::Session().run( argc, argv );
+    return result;
+}
+
