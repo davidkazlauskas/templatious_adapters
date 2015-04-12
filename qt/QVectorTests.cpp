afb7d7cc8938f4e0f916c6c38768cc9f06295ecb
David <david@templatious.org>
2015-04-12 20:46:34 +0300

    multiple for reduction

---
+++ qt/QVectorTests.cpp
@@ -0,0 +1,25 @@
+
+void qtTests() {}
+
+#ifndef NO_QT_TEST_SUITE
+
+// Qt vectors are implicitly shared
+// and don't have rvalue constructor...
+#define TTEST_SKIP_PROXY_MOVE_SEMANTICS
+
+// QVector relies on objects having a
+// default constructor... Not good
+#define TTEST_SKIP_MISC_MOVE_TEST
+
+#include "../catch.hpp"
+#include "../TestDefs.hpp"
+#include "All.hpp"
+
+TEST_CASE( "QVector", "[qvector]" )
+{
+    std::cout << "Running QVector tests." << std::endl;
+    bool res = tt::fullRun<QVector,std::allocator>();
+    REQUIRE( res );
+}
+
+#endif
