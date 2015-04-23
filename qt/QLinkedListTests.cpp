af891cf0b493bc2adbae0cabf5ece08b35a34509
David <david@templatious.org>
2015-04-23 19:33:12 +0300

    clangin

---
+++ qt/QLinkedListTests.cpp
@@ -0,0 +1,21 @@
+
+void qList() {}
+
+#ifndef NO_QT_TEST_SUITE
+
+// Qt lists are implicitly shared
+// and don't have rvalue constructor...
+#define TTEST_SKIP_PROXY_MOVE_SEMANTICS
+
+#include "../catch.hpp"
+#include "../TestDefs.hpp"
+#include "All.hpp"
+
+TEST_CASE( "QLinkedList", "[qlist]" )
+{
+    std::cout << "Running QLinkedList tests." << std::endl;
+    bool res = tt::fullRun<QLinkedList,std::allocator>();
+    REQUIRE( res );
+}
+
+#endif
