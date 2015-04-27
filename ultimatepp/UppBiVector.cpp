c104f25316a3c7fceeb974eb29a44bd766ff8645
David <david@templatious.org>
2015-04-27 19:21:54 +0300

    good enough

--- ultimatepp/UppBiVector.cpp
+++ ultimatepp/UppBiVector.cpp
@@ -1,7 +1,11 @@
 
-void uppTests() {}
+void uppBiVectorTest() {}
 
 #ifndef NO_UPP_TEST_SUITE
+
+#define TTEST_SKIP_UPP_BIVECTOR // is it worth it? I don't think so
+
+#ifndef TTEST_SKIP_UPP_BIVECTOR
 
 // requires wrapping type in some
 // nonsense to be contained in the vector
@@ -22,5 +26,7 @@ TEST_CASE( "Upp::BiVector", "[upp_bivector]" )
     bool res = tt::fullRun<Upp::BiVector,std::allocator>();
     REQUIRE( res );
 }
+
+#endif
 
 #endif
